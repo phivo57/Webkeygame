9f3f1532a26a6a004c350f0ad12da1b8bb3f92b8
Phivo97 <44152463+Phivo97@users.noreply.github.com>
2018-12-19 12:06:54 +0700

    Create header.php

---
+++ view/v_trangchu/header.php
@@ -0,0 +1,116 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TPQShopKeyGame | <?php echo $title?></title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.0.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+<script type="text/javascript">
+$(function () {
+	
+	var filterList = {
+	
+		init: function () {
+		
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixitup({
+				targetSelector: '.portfolio',
+				filterSelector: '.filter',
+				effects: ['fade'],
+				easing: 'snap',
+				// call the hover effect
+				onMixEnd: filterList.hoverEffect()
+			});				
+		
+		},
+		
+		hoverEffect: function () {
+		
+			// Simple parallax effect
+			$('#portfoliolist .portfolio').hover(
+				function () {
+					$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+					$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+				},
+				function () {
+					$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+					$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+				}		
+			);				
+		
+		}
+
+	};
+	
+	// Run the show!
+	filterList.init();
+	
+	
+});	
+</script>
+</head>
+<body>
+<!-- Header Starts Here -->
+<div class="header">
+	<div class="container">
+		<div class="logo">
+			<a href="."><img src="images/logo.png" alt=""></a>
+		</div>
+		<span class="menu"></span>
+		<div class="navigation">
+			<ul class="navig cl-effect-3" >
+				<li><a href=".">Trang Chủ</a></li>
+				<li><a href="games.php">Games</a></li>
+				<li><a href="faq.php">FAQ</a></li>
+				<li>
+					<?php
+						if(isset($_SESSION['ten_khach_hang']))
+						{						
+							echo "<a href='logout.php'>Thoát</a>";
+						}
+						else
+						{
+							echo "<a href='login.php'>Đăng nhập</a>";
+						}
+					?>
+				</li>
+			</ul>
+			<div class="clearfix"></div>
+			<script>
+				$( "span.menu" ).click(function() {
+				  $( ".navigation" ).slideToggle( "slow", function() {
+				    // Animation complete.
+				  });
+				});
+			</script>
+
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<div class="banner"></div>
+<!-- Header Ends Here -->
+<div class="banner-bot">
+	<div class="container">
+		<h2>Chào mừng bạn <?php if(isset($_SESSION["ten_khach_hang"]))echo $_SESSION["ten_khach_hang"]?> đến với Shop!</h2>
+		<p>Chuyên cung cấp key game cho người việt - Web hỗ trợ người Việt Nam dễ tiếp cận với game có bản quyền</p>
+	</div>
+</div>
