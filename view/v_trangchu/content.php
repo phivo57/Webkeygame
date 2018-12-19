ecd8247c853a0868f118b51729c0cbbd1ec06126
Phivo97 <44152463+Phivo97@users.noreply.github.com>
2018-12-19 12:07:43 +0700

    Trang chủ

---
+++ view/v_trangchu/content.php
@@ -0,0 +1,51 @@
+<!-- Gallery Starts Here -->
+<div class="gallery">
+	<div class="container">
+		<h3>Game nổi bật</h3>
+		<div class="gallery-top">
+					<div id="portfoliolist">
+					<div class="portfolio app mix_all" data-cat="app" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper">		
+							<a href="chi_tiet_games.php?ma_game=4" class="b-link-stripe b-animate-go  thickbox">
+						     <img src="images/pic1.jpeg" />
+							 <div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "> </h2>
+						  	</div>
+							</a>
+		                </div>
+					</div>				
+					<div class="portfolio icon mix_all" data-cat="icon" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper">		
+							<a href="chi_tiet_games.php?ma_game=1" class="b-link-stripe b-animate-go  thickbox">
+						     <img src="images/pic2.jpg" /><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "> </h2>
+						  	 </div></a>
+		                </div>
+					</div>		
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper">		
+							<a href="chi_tiet_games.php?ma_game=7" class="b-link-stripe b-animate-go  thickbox">
+						     <img src="images/pic3.jpg" /><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "> </h2>
+						  	 </div></a>
+		                </div>
+					</div>				
+					<div class="portfolio logos mix_all" data-cat="logo" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper">		
+							<a href="chi_tiet_games.php?ma_game=9" class="b-link-stripe b-animate-go  thickbox">
+						     <img src="images/pic4.jpg" /><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "> </h2>
+						  	 </div></a>
+		                </div>
+					</div>	
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper">		
+							<a href="chi_tiet_games.php?ma_game=2" class="b-link-stripe b-animate-go  thickbox">
+						     <img src="images/pic5.jpg" /><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "> </h2>
+						  	 </div></a>
+		                </div>
+					</div>			
+				</div>
+
+		</div>
+	</div>
+</div>
+<!-- Gallery Ends Here -->
+<!-- Video Part starts Here -->
+<!-- Video Part Ends Here -->
