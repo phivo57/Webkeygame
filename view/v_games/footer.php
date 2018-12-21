afada8addd02a1005a1256fab5db5f0d8c34d37a
vongtrican <44152551+vongtrican@users.noreply.github.com>
2018-12-21 11:44:05 +0700

    Add files via upload

---
+++ view/v_games/footer.php
@@ -0,0 +1,15 @@
+<!-- Footer Starts Here -->
+<div class="footer">
+	<div class="container">
+		<ul class="social">
+			<li><i class="fa"></i></li>
+			<li><i class="fb"></i></li>
+			<li><i class="fc"></i></li>
+		</ul>
+		<p>2014 Design by <a href="http://w3layouts.com">W3layouts</a></p>
+	</div>
+	
+</div>
+<!-- Footer Ends Here -->
+</body>
+</html>
